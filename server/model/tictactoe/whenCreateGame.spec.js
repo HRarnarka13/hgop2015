5adef19e8f30f82c6c082a9ea38796f8a58ed889
Arnar Kari <arnarkari93@gmail.com>
2015-12-07 19:48:53 +0000

    align code

--- server/model/tictactoe/whenCreateGame.spec.js
+++ server/model/tictactoe/whenCreateGame.spec.js
@@ -6,17 +6,17 @@ describe('when initializing game', () => {
     it('should initialize game', () => {
         given = [];
         when = {
-            id:'1234',
-            command:'InitalizeGame',
-            userName:'ArnarKari',
-            name:'Tictactoe',
-            timeStamp: "2015.12.04T18:35:51"
+            id       : '1234',
+            command  : 'InitalizeGame',
+            userName : 'ArnarKari',
+            name     : 'Tictactoe',
+            timeStamp: '2015.12.04T18:35:51'
         };
         then = [{
-            id:"1234",
-            event:'GameInitialized',
-            userName:'ArnarKari',
-            timeStamp: "2015.12.04T18:35:51"
+            id       : '1234',
+            event    : 'GameInitialized',
+            userName : 'ArnarKari',
+            timeStamp: '2015.12.04T18:35:51'
         }];
 
         var event = tictactoeCommandHandler(given).execudeCommand(when);
@@ -27,17 +27,17 @@ describe('when initializing game', () => {
     it('should initialize a different gane', () => {
         given = [];
         when = {
-            id:'0987',
-            command:'InitalizeGame',
-            userName:'MikeCohn',
-            name:'Tictactoe',
+            id       : '0987',
+            command  : 'InitalizeGame',
+            userName : 'MikeCohn',
+            name     : 'Tictactoe',
             timeStamp: "2015.12.04T19:36:51"
         };
         then = [{
-            id:"0987",
-            event:'GameInitialized',
-            userName:'MikeCohn',
-            timeStamp: "2015.12.04T19:36:51"
+            id       : '0987',
+            event    : 'GameInitialized',
+            userName : 'MikeCohn',
+            timeStamp: '2015.12.04T19:36:51'
         }];
         var actualEvents = tictactoeCommandHandler(given).execudeCommand(when);
         JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
