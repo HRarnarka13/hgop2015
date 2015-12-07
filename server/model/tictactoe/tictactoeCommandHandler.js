3d713c17180c9465f367a53a087705dfc96c1e3b
Arnar Kari <arnarkari93@gmail.com>
2015-12-07 19:49:44 +0000

    added test for joining game that dose not exsist

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -5,18 +5,27 @@ module.exports = function tictactoeCommandHandler(events) {
             switch (command.command) {
                 case 'InitalizeGame':
                     return [{
-                        id: command.id,
-                        event: 'GameInitialized',
-                        userName: command.userName,
+                        id       : command.id,
+                        event    : 'GameInitialized',
+                        userName : command.userName,
                         timeStamp: command.timeStamp
                     }];
                 case 'JoinGame':
+                    console.log(events);
+                    if (typeof events === 'undefined' || events.length <= 0) {
+                        return [{
+                            id       : command.id,
+                            event    : 'GameDoesNotExist',
+                            userName : command.userName,
+                            timeStamp: command.timeStamp
+                        }];
+                    }
                     return [{
-                        id: command.id,
-                        event: 'GameJoined',
-                        userName: command.userName,
+                        id              : command.id,
+                        event           : 'GameJoined',
+                        userName        : command.userName,
                         opponentUserName: events[0].userName,
-                        timeStamp: command.timeStamp
+                        timeStamp       : command.timeStamp
                     }];
                 default:
                     throw new Error('No matching command found: ' + command.command);
--- server/model/tictactoe/whenJoiningGame.spec.js
+++ server/model/tictactoe/whenJoiningGame.spec.js
@@ -5,27 +5,47 @@ describe('when joining game', () => {
 
     it('should join game', () => {
         given = [{
-            id: '111',
-            event: 'GameInitialized',
-            userName: 'ArnarKari',
-            timeStamp: "2015.12.04T18:35:51"
+            id       : '111',
+            event    : 'GameInitialized',
+            userName : 'ArnarKari',
+            timeStamp: '2015.12.04T18:35:51'
         }];
         when = {
-            id: '111',
-            command: 'JoinGame',
-            userName: 'MikeCohn',
-            name: 'Tictactoe',
-            timeStamp: "2015.12.04T18:36:01"
+            id       : '111',
+            command  : 'JoinGame',
+            userName : 'MikeCohn',
+            name     : 'Tictactoe',
+            timeStamp: '2015.12.04T18:36:01'
         };
         then = [{
-            id: '111',
-            event: 'GameJoined',
-            userName: 'MikeCohn',
+            id              : '111',
+            event           : 'GameJoined',
+            userName        : 'MikeCohn',
             opponentUserName: 'ArnarKari',
-            timeStamp: "2015.12.04T18:36:01"
+            timeStamp       : '2015.12.04T18:36:01'
         }];
 
         var event = tictactoeCommandHandler(given).execudeCommand(when);
         JSON.stringify(event).should.be.exactly(JSON.stringify(then));
     });
+
+    it('should not join game because it dosen\'t exsist', () => {
+        given = [];
+        when = {
+            id       : '222',
+            command  : 'JoinGame',
+            userName : 'CohnMike',
+            name     : 'Tictactoe',
+            timeStamp: '2015.12.04T18:40:07'
+        };
+        then = [{
+            id       : '222',
+            event    : 'GameDoesNotExist',
+            userName : 'CohnMike',
+            timeStamp: '2015.12.04T18:40:07'
+        }];
+
+        var event = tictactoeCommandHandler(given).execudeCommand(when);
+        JSON.stringify(event).should.be.exactly(JSON.stringify(then));
+    })
 });
