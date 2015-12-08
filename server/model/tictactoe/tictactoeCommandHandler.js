3ac0d077b35195ae260d68b0fdb09bcd35ea3acb
Arnar Kari <arnarkari93@gmail.com>
2015-12-08 12:11:45 +0000

    added another movemade test, refactored the commandHandler

--- server/model/tictactoe/tictactoeCommandHandler.js
+++ server/model/tictactoe/tictactoeCommandHandler.js
@@ -1,6 +1,16 @@
 var _ = require('lodash');
 module.exports = function tictactoeCommandHandler(events) {
     const gameCreatedEvent = events[0];
+    const board = [['','',''],['','',''],['','','']]
+
+    _.each(events, (e) => {
+        if (e.event === 'MoveMade') {
+            const row = e.move.x;
+            const column = e.move.y;
+            board[row][column] = e.move.symbol;
+        }
+    })
+
     const handlers = {
         'InitalizeGame' : (cmd) => {
             {
@@ -33,12 +43,27 @@ module.exports = function tictactoeCommandHandler(events) {
         },
         'Move' : (cmd) => {
             {
+                const row = cmd.move.x;
+                const column = cmd.move.y
+                console.log('Before:', board);
+
+                if (board[row][column] !== '') {
+                    return [{
+                        id : cmd.id,
+                        event : 'IllegalMove',
+                        userName : cmd.userName,
+                        move : cmd.move,
+                        timeStamp : cmd.timeStamp
+                    }]
+                }
+
+                board[row][column] = cmd.move.symbol;
+                console.log('After:',board);
                 return [{
                     id : cmd.id,
                     event : 'MoveMade',
                     userName : cmd.userName,
-                    cell : cmd.cell,
-                    symbol : cmd.symbol,
+                    move : cmd.move,
                     timeStamp : cmd.timeStamp
                 }]
             }
--- server/model/tictactoe/whenMakingMove.spec.js
+++ server/model/tictactoe/whenMakingMove.spec.js
@@ -2,7 +2,8 @@
 var tictactoeCommandHandler = require('./tictactoeCommandHandler');
 describe('when making move', () => {
     var given, when, then;
-    it('should make move', () => {
+
+    beforeEach(() => {
         given = [{
             id       : '333',
             event    : 'GameInitialized',
@@ -16,25 +17,73 @@ describe('when making move', () => {
             opponentUserName: 'ArnarKari',
             timeStamp       : '2015.12.04T19:44:59'
         }];
+    });
 
+    it('should make move', () => {
         when = {
             id        : '333',
             command   : 'Move',
             userName  : 'ArnarKari',
-            cell      : 1,
-            symbol    : 'x',
+            move : {
+                x      : 0,
+                y      : 1,
+                symbol : 'x',
+            },
             timeStamp : '2015.12.04T19:45:04'
         };
 
         then = [{
-            id        : '333',
-            event     : 'MoveMade',
-            userName  : 'ArnarKari',
-            cell      : 1,
-            symbol    : 'x',
+            id       : '333',
+            event    : 'MoveMade',
+            userName : 'ArnarKari',
+            move : {
+                x : 0,
+                y : 1,
+                symbol : 'x',
+            },
             timeStamp : '2015.12.04T19:45:04'
         }];
 
+        var actualEvents = tictactoeCommandHandler(given).execudeCommand(when);
+        JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
+    });
+
+    it('should reject illegal move', () => {
+        given.push({
+            id       : '333',
+            event    : 'MoveMade',
+            userName : 'ArnarKari',
+            move : {
+                x      : 0,
+                y      : 1,
+                symbol : 'x',
+            },
+            timeStamp : '2015.12.04T19:45:04'
+        });
+
+        when = {
+            id       : '333',
+            command  : 'Move',
+            userName : 'ArnarKari',
+            move : {
+                x : 0,
+                y : 1,
+                symbol : 'x',
+            },
+            timeStamp : '2015.12.04T19:45:04'
+        };
+
+        then = [{
+            id : '333',
+            event : 'IllegalMove',
+            userName : 'ArnarKari',
+            move : {
+                x : 0,
+                y : 1,
+                symbol : 'x',
+            },
+            timeStamp : '2015.12.04T19:45:04'
+        }];
         var actualEvents = tictactoeCommandHandler(given).execudeCommand(when);
         JSON.stringify(actualEvents).should.be.exactly(JSON.stringify(then));
     });
