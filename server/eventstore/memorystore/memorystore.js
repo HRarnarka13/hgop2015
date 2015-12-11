0ce7609f9d71d58f1a181a028125a6072294aafc
Arnar Kari <arnarkari93@gmail.com>
2015-12-11 16:41:55 +0000

    added eventstore

---
+++ server/eventstore/memorystore/memorystore.js
@@ -0,0 +1,18 @@
+var q = require('q');
+module.exports = function(){
+    var store = {};
+    return {
+        loadEvents : function(id){
+            console.log('id', id);
+            var deferred = q.defer();
+            deferred.resolve(store[id] || []);
+            return deferred.promise;
+        },
+        storeEvents: function(id, events){
+            var deferred = q.defer();
+            store[id] = (store[id] || []).concat(events);
+            deferred.resolve(store[id]);
+            return deferred.promise;
+        }
+    };
+}
