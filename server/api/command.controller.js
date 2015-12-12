a41368a31ab879fd10e0786933e6e5d861127b6d
Arnar Kari <arnarkari93@gmail.com>
2015-12-12 11:33:13 +0000

    added command controller

---
+++ server/api/command.controller.js
@@ -0,0 +1,27 @@
+'use strict';
+var _                = require('lodash');
+var boundedContext   = require('../model/tictactoe/tictactoeBoundedContext');
+var tictactoeHandler = require('../model/tictactoe/tictactoeCommandHandler');
+var app              = require('../app');
+
+module.exports = (store) => {
+    return {
+        executeCommand: (req, res) => {
+            try {
+                var context = boundedContext(store, tictactoeHandler);
+                console.log('context', context);
+                console.log('req.body',req.body);
+                context.handleCommand(req.body).then((result) => {
+                    console.log('result', result);
+                    res.json(result);
+                }, (err) => {
+                    console.log('err', err);
+                    res.json(err);
+                });
+            }
+            catch (exception) {
+                res.json(exception)
+            }
+        }
+    };
+};
