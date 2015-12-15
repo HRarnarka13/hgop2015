f4fd5de9b4e0dd727273c81c22fed217c594f395
Arnar Kari <arnarkari93@gmail.com>
2015-12-15 17:05:31 +0000

    add port number to deploy script, enabling having multible instances running on diffrent ports

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ "$#" -ne 4 ]; then
+  echo "Usage: $0 [IP address] [container] [git-hash] [port]"
+  exit 1
+fi
+
 # Pull from docker on the test machine.
 testport="vagrant@$1"
-ssh $testport 'bash -s' < test-deploy.sh $2 $3
+ssh $testport 'bash -s' < test-deploy.sh $2 $3 $4
