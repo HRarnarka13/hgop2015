b44296cc04609c25ce47c2d0ae9843909beef34f
Arnar Kari <arnarkari93@gmail.com>
2015-12-15 16:08:58 +0000

    update order of paramenters and added parameter for git version

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Push to docker
-docker push $1
-
 # Pull from docker on the test machine.
-testport="vagrant@$2"
-ssh $testport 'bash -s' < test-deploy.sh $1 $2
+testport="vagrant@$1"
+ssh $testport 'bash -s' < test-deploy.sh $2 $3
