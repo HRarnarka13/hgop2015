818ce25a66e6a349595403f3e3d39092c8d887e1
Arnar Kari <arnarkari93@gmail.com>
2015-12-16 16:20:02 +0000

    update capacity test

--- server/capacity/tictactoe.load.js
+++ server/capacity/tictactoe.load.js
@@ -1,9 +1,9 @@
 var user = require('../fluid-api/tictactoeFluid').user;
 var given = require('../fluid-api/tictactoeFluid').given;
 
-it('Should play 200 games in 6 seconds.', function (done) {
+it('Should play 1000 games in 6 seconds.', function (done) {
     var doneCount = 0;
-    var gamesToPlay = 200;
+    var gamesToPlay = 1000;
     var x = 6;
 
     this.timeout(x * 1000);
